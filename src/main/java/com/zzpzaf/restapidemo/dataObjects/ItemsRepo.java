7f326031cedb289e6cbb9423640686acc797e440
Panos Zafeiropoulos <zzpzaf@gmail.com>
2022-05-22 15:47:12 +0300

    main commit after added mysql / jdbc

--- src/main/java/com/zzpzaf/restapidemo/dataObjects/ItemsRepo.java
+++ src/main/java/com/zzpzaf/restapidemo/dataObjects/ItemsRepo.java
@@ -1,8 +1,11 @@
 package com.zzpzaf.restapidemo.dataObjects;
 
-import java.util.ArrayList;
+//import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -10,14 +13,14 @@ public class ItemsRepo {
     
     //private Item item = new Item();
    
+    @Autowired
+    private JdbcTemplate jdbcTemplate; 
 
+    
     public List<Item> getItems() {
-        List<Item> itemsList =  new ArrayList<>();
+        //List<Item> itemsList =  new ArrayList<>();
 
-        itemsList.add(new Item(1, "Pencil", 3, 1998, 2.65 ));
-        itemsList.add(new Item(2, "Notebook", 3, 2005, 3.75 ));
-        itemsList.add(new Item(3, "Eraser Gum", 6, 2017, 1.05 ));
+        return jdbcTemplate.query("SELECT * FROM items", BeanPropertyRowMapper.newInstance(Item.class));
 
-        return itemsList;
     }  
 }
